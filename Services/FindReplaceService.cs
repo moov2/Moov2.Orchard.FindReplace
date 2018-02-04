37d6ac7bd943f07383c637ae12580feb8663e274
Peter Keating <peter@moov2.com>
2018-02-04 15:17:42 +0000

    Update last modified date/by for content items

--- Services/FindReplaceService.cs
+++ Services/FindReplaceService.cs
@@ -1,15 +1,19 @@
 ﻿using Orchard;
 using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
 using Orchard.Data;
 using Orchard.Environment.Configuration;
+using Orchard.Security;
+using Orchard.Services;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 
 namespace Moov2.Orchard.FindReplace.Services {
     public class FindReplaceService : IFindReplaceService {
         #region Dependencies
 
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IClock _clock;
         private readonly IContentManager _contentManager;
         private readonly ITransactionManager _transactionManager;
         private readonly ShellSettings _shellSettings;
@@ -18,7 +22,9 @@ namespace Moov2.Orchard.FindReplace.Services {
 
         #region Constructor
 
-        public FindReplaceService(IContentManager contentManager, ShellSettings shellSettings, ITransactionManager transactionManager) {
+        public FindReplaceService(IAuthenticationService authenticationService, IClock clock, IContentManager contentManager, ShellSettings shellSettings, ITransactionManager transactionManager) {
+            _authenticationService = authenticationService;
+            _clock = clock;
             _contentManager = contentManager;
             _shellSettings = shellSettings;
             _transactionManager = transactionManager;
@@ -41,19 +47,34 @@ namespace Moov2.Orchard.FindReplace.Services {
         public void Replace(IList<int> itemIds, string find, string replace)
         {
             var tableName = string.IsNullOrEmpty(_shellSettings.DataTablePrefix) ? "Orchard_Framework_ContentItemVersionRecord" : string.Format("{0}_Orchard_Framework_ContentItemVersionRecord", _shellSettings.DataTablePrefix);
+            var session = _transactionManager.GetSession();
 
-            _transactionManager.GetSession()
-                .CreateSQLQuery(string.Format("update {0} set [Data]=REPLACE([Data], '{1}', '{2}') WHERE Latest=1 AND ContentItemRecord_Id IN ({3})", tableName, find, replace, string.Join(",", itemIds)))
+            session.CreateSQLQuery(string.Format("update {0} set [Data]=REPLACE([Data], '{1}', '{2}') WHERE Latest=1 AND ContentItemRecord_Id IN ({3})", tableName, find, replace, string.Join(",", itemIds)))
                 .ExecuteUpdate();
 
-            _transactionManager.GetSession()
-                .CreateSQLQuery(string.Format("update {0} set [Data]=REPLACE([Data], '{1}', '{2}') WHERE Latest=1 AND ContentItemRecord_Id IN ({3})", tableName, HttpUtility.UrlEncode(find), HttpUtility.UrlEncode(replace), string.Join(",", itemIds)))
+            session.CreateSQLQuery(string.Format("update {0} set [Data]=REPLACE([Data], '{1}', '{2}') WHERE Latest=1 AND ContentItemRecord_Id IN ({3})", tableName, HttpUtility.UrlEncode(find), HttpUtility.UrlEncode(replace), string.Join(",", itemIds)))
                 .ExecuteUpdate();
+
+            var contentItems = _contentManager.GetMany<ContentItem>(itemIds, VersionOptions.Published, QueryHints.Empty);
+            var utcNow = _clock.UtcNow;
+
+            foreach (var contentItem in contentItems)
+            {
+                contentItem.As<CommonPart>().ModifiedUtc = utcNow;
+                contentItem.As<CommonPart>().VersionModifiedUtc = utcNow;
+                contentItem.As<CommonPart>().VersionModifiedBy = GetUserName();
+            }
         }
 
         #endregion
 
         #region
+
+        private string GetUserName()
+        {
+            var user = _authenticationService.GetAuthenticatedUser();
+            return user == null ? string.Empty : user.UserName;
+        }
 
         /// <summary>
         /// Escapes any % values that are used in the LIKE expression
